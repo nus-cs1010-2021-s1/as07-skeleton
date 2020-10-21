744abeff871915de74981ccb531601361f32f4b1
Ooi Wei Tsang <weitsang@yahoo.com>
2020-10-21 23:04:59 +0000

    First commit

---
+++ inversion.c
@@ -0,0 +1,16 @@
+/**
+ * CS1010 Semester 1 AY20/21
+ * Assignment 7: Inversion
+ *
+ * TODO(by student): Brief description of the purpose 
+ * of this program.
+ *
+ * @file: inversion.c
+ * @author: XXX (Group XXX)
+ */
+
+#include "cs1010.h"
+
+int main()
+{
+}
---
+++ sort.c
@@ -0,0 +1,15 @@
+/**
+ * CS1010 Semester 1 AY20/21
+ * Assignment 7: Sort
+ *
+ * TODO(by student): Brief description of the purpose 
+ * of this program.
+ *
+ * @file: sort.c
+ * @author: XXX (Group XXX)
+ */
+#include "cs1010.h"
+
+int main()
+{
+}
